40e51f3c8747bd45cded567b980058b39c59e529
Bobo Chen <bobochen@kkbox.com>
2016-03-14 12:31:34 +0800

    Implement PotterShoppingCart and test passed. (Green Light).

--- src/PotterShoppingCart.php
+++ src/PotterShoppingCart.php
@@ -5,6 +5,9 @@ namespace Tdd91;
 
 class PotterShoppingCart
 {
+    protected $books = [];
+
+    protected $total_price = 0;
 
     /**
      * PotterShoppingCart constructor.
@@ -13,16 +16,26 @@ class PotterShoppingCart
     {
     }
 
-    public function add($param, $buy_n)
+    public function add(Book $book, $buy_n)
     {
+        if (0 == $buy_n) return false;
+
+        for ($i = 1; $i <= $buy_n; $i++) {
+            $this->books[] = $book;
+        }
     }
 
     public function checkOut()
     {
+        $count_books = count($this->books);
+        $this->total_price = 100 * $count_books;
+        if (2 == $count_books) {
+            $this->total_price *= 0.95;
+        }
     }
 
     public function getTotal()
     {
-        return 100;
+        return $this->total_price;
     }
 }
