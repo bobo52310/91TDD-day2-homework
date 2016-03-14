b1ec04237c2ddb326ef09273718084f8ae5b61cb
Bobo Chen <bobochen@kkbox.com>
2016-03-14 17:56:00 +0800

    Modify for Scenario 6  and test passed. (Green Light).

--- src/Book.php
+++ src/Book.php
@@ -5,12 +5,21 @@ namespace Tdd91;
 
 class Book
 {
+    protected $series_id;
+    protected $price;
 
     /**
      * Book constructor.
      * @param array $array
      */
-    public function __construct($array)
+    public function __construct(Array $arg)
     {
+        $this->series_id = $arg['series_id'];
+        $this->price = $arg['price'];
+    }
+
+    public function getSeriesId()
+    {
+        return $this->series_id;
     }
 }
--- src/PotterShoppingCart.php
+++ src/PotterShoppingCart.php
@@ -20,26 +20,50 @@ class PotterShoppingCart
     {
         if (0 == $buy_n) return false;
 
-        for ($i = 1; $i <= $buy_n; $i++) {
-            $this->books[] = $book;
+        for ($i = 1; $i <= $buy_n ; $i++) {
+            $this->books[] = $book->getSeriesId();
         }
     }
 
     public function checkOut()
     {
-        $count_books = count($this->books);
-        $this->total_price = 100 * $count_books;
-        if (2 == $count_books) {
-            $this->total_price *= 0.95;
-        } elseif (3 == $count_books) {
-            $this->total_price *= 0.9;
-        } elseif (4 == $count_books) {
-            $this->total_price *= 0.8;
-        } elseif (5 == $count_books) {
-            $this->total_price *= 0.75;
+        // 將 books 進行分群組
+        $book_grouped = $this->groupBooks($this->books);
+
+        foreach ($book_grouped as $books) {
+            $count_books = count($books);
+            $group_price = 100 * $count_books;
+            if (2 == $count_books) {
+                $group_price *= 0.95;
+            } elseif (3 == $count_books) {
+                $group_price *= 0.9;
+            } elseif (4 == $count_books) {
+                $group_price *= 0.8;
+            } elseif (5 == $count_books) {
+                $group_price *= 0.75;
+            }
+            $this->total_price += $group_price;
         }
     }
 
+    private function groupBooks($arr)
+    {
+        $arr_unique = [];
+        while ($arr != array_unique($arr)) {
+            $arr_unique[] = array_unique($arr);
+            $last_key = count($arr_unique) - 1;
+
+            // 移除掉已經 unique 出來的元素
+            foreach ($arr_unique[$last_key] as $del_val) {
+                if(($key = array_search($del_val, $arr)) !== false) {
+                    unset($arr[$key]);
+                }
+            }
+        }
+
+        $arr_unique[] = $arr; // 已經完成分組
+        return $arr_unique;
+    }
     public function getTotal()
     {
         return $this->total_price;
